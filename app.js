69d008504bfa7b375a25c72fa38c33771fccaec3
JorgeHernandez <jorge.hernandez64@outlook.es>
2018-08-17 13:59:36 +0200

    added some jquery functionality

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ g.greet().setLang('es').greet(true);//Saludos, John Doe
 //g.greet().setLang('fr').greet(true);//Invalid langage
 g.greet().setLang('es').greet(true).log();//Inició sesión: John Doe
 
+
+$('#login').click(function(){
+	var gx = G$('Jane','Smith');
+	$('#logindiv').hide();
+	gx.setLang($('#lang').val()).HTMLgreeting('greeting',true).log();
+});
